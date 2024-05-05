0425951b1b0ab5d110c0c619e4ce55eaba2f891a
yash-not-bot22 <yashbaghel.aai@gmail.com>
2024-05-05 10:59:17 +0530

    q

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ dotenv.config();
 const app = express();
 app.get("/",(req,res)=>{
   res.send("server running");
-})
+});
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
